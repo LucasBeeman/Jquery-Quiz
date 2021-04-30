6babdf87ee3f8fc532abfd7dcbb17aed041007d6
Beeman_Lucas <Beeman_Lucas@student.mahoningctc.com>
2021-04-30 09:16:53 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,30 @@
+$(document).ready(() =>{
+    $(".hint-box").on("click", () => {
+      $(".hint").slideToggle(500)
+    })
+    $(".wrong-answer-one").on("click", () => {
+      $(".wrong-text-one").fadeOut(500)
+      $(".wrong-text-two").fadeOut(500)
+      $(".wrong-text-three").fadeOut(500)
+      $(".frown").show()
+    })
+    $(".wrong-answer-two").on("click", () => {
+      $(".wrong-text-one").fadeOut(500)
+      $(".wrong-text-two").fadeOut(500)
+      $(".wrong-text-three").fadeOut(500)
+      $(".frown").show()
+    })
+    $(".wrong-answer-three").on("click", () => {
+      $(".wrong-text-one").fadeOut(500)
+      $(".wrong-text-two").fadeOut(500)
+      $(".wrong-text-three").fadeOut(500)
+      $(".frown").show()
+    })
+     $(".correct-answer").on("click", () => {
+      $(".frown").hide()
+      $(".smiley").show()
+      $(".wrong-text-one").fadeOut(500)
+      $(".wrong-text-two").fadeOut(500)
+      $(".wrong-text-three").fadeOut(500)
+    })
+  });
